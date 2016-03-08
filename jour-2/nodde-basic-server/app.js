c672dba79d2a69908d0be688a0eb08ef4eaa74c0
leojpod <leo.jpod@gmail.com>
2016-03-08 13:02:55 +0100

    app.js

--- jour-2/nodde-basic-server/app.js
+++ jour-2/nodde-basic-server/app.js
@@ -5,52 +5,63 @@
 var http = require('http');
 var qs = require('querystring');
 var url = require('url');
-var routing = require('./routing.js').routing;
+var routing = require('./routing.js');
+
+var corsHeaders = {
+  "Access-Control-Allow-Origin": "http://localhost:4200",
+  "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Credentials": true,
+  "Access-Control-Max-Age": '86400', // 24 hours
+  "Access-Control-Allow-Headers":
+    "X-Requested-With, Access-Control-Allow-Origin," +
+    " X-HTTP-Method-Override, Content-Type, Authorization, Accept, x-access-token"
+};
 
 http.createServer(function(request, response) {
-  var corsHeaders = {
-    "Access-Control-Allow-Origin": "http://localhost:4200",
-    "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Credentials": true,
-    "Access-Control-Max-Age": '86400', // 24 hours
-    "Access-Control-Allow-Headers": "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept, x-access-token"
-  };
   for (var header in corsHeaders) {
     if (corsHeaders.hasOwnProperty(header)) {
       response.setHeader(header, corsHeaders[header]);
     }
   }
+  //notre API fonctionne en JSON: on peut fixer le header une fois pour toute
   response.setHeader('Content-Type', 'application/json');
-  switch (request.method) {
-    case 'GET':
-      var reqUrl = url.parse(request.url, true);
-      request.query = reqUrl.query;
-      routing.routeRequest(request, response);
-      break;
-    case 'POST':
-      var bodyString = '';
-      request.on('data', function(data) {
-        bodyString += data;
-        if (bodyString.length > 1e6) {
-          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-          request.connection.destroy();
-        }
-      });
-      request.on('end', function() {
-        if (request.headers['content-type'] == 'application/x-www-form-urlencoded') {
-          request.post = qs.parse(bodyString);
-        } else if (request.headers['content-type'] == 'application/json') {
-          request.post = JSON.parse(bodyString);
-        }
-        routing.routeRequest(request, response);
-      });
-      break;
-    case 'OPTIONS':
-      console.log('OPTIONS call!');
-      response.writeHead(200);
-      response.end();
-      break;
-    default:
-      routing.routeRequest(request, response);
+
+  if (request.method == "OPTIONS") {
+    response.writeHead(200);
+    response.end();
+    return;
   }
+
+  var bodyString = '';
+  request.on('data', function (chunk) {
+    bodyString += chunk;
+    if (bodyString.length > 1e6) {
+      // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+      request.connection.destroy();
+    }
+  });
+  request.on('end', function () {
+    var decodedBody;
+    if (bodyString.length >= 0) {
+      //there was a body string
+      switch (request.headers['content-type']) {
+        case 'application/x-www-form-urlencoded':
+          decodedBody = qs.parse(bodyString);
+          break;
+        case 'application/json':
+          decodedBody = JSON.parse(bodyString);
+          break;
+        default:
+          decodedBody = bodyString;
+      }
+    }
+    var reqUrl = url.parse(request.url);
+    request.url = reqUrl;
+    request.query = reqUrl.query;
+    request.body = decodedBody;
+    routing.routeRequest(request, response);
+  });
+  //    routing.routeRequest(request, response);
+  // ! un cas particuler: si la requête est de type OPTIONS:
+  // pas la peine d'aller plus loin, il suffit d'envoyer les headers CORS
 }).listen(8888);
