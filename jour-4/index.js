4346e67e7743d590a36d643c8bae3ac3ec9b8d3f
leojpod <leo.jpod@gmail.com>
2016-03-10 08:54:46 +0100

    a few more stuff

---
+++ jour-4/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by leojpod on 3/10/16.
+ */
+var async = require('async');
+var MongoClient= require('mongodb').MongoClient;
+
+MongoClient.connect('url', function (err, db) {
+
+});
