fd45b46bff2eb2a1ce3ae55b2fe74799143c9e70
Richardson Santos <rpsantosvix@gmail.com>
2016-03-10 11:58:04 +0100

    asdad

--- jour-4/index.js
+++ jour-4/index.js
@@ -4,6 +4,22 @@
 var async = require('async');
 var MongoClient= require('mongodb').MongoClient;
 
-MongoClient.connect('url', function (err, db) {
+var db;
+
+var url = "mongodb://localhost:27017";
+
+MongoClient.connect( url + '/client', function (err, db) {
+
+  var nouveauClients = [
+      { 'clientId': "1", 'nom': 'Richardson', 'depenses': '300' },
+      { 'clientId': "2", 'nom': 'Fred', 'depenses': '600'},
+      { 'clientId': "3", 'nom': 'Alex', 'depenses': '400'},
+      { 'clientId': "4", 'nom': 'Niah', 'depenses': '500'},
+      { 'clientId': "5", 'nom': 'Léo', 'depenses': '1000'}
+    ];
+
+  db.collection('client').insertMany(nouveauClients, function (err, results){
+    console.log(results);
+  });
 
 });
